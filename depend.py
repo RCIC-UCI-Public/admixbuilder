d561f6392b2c7d6ca7da60ba0c89c1802f33017c
Nadya Williams <nadya.williams@gmail.com>
2022-07-21 13:02:56 -0700

    add font style and colors for easier reading add ranking for the admixes so that the resulting graph has less white spaces and shorter edges for a better readability

--- depend.py
+++ depend.py
@@ -18,8 +18,7 @@ class Node(object):
            self.provides = admixes[name]['provides']
         self.labelheader = 'label=<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">'
         self.labeltrailer = "</TABLE>>"
-        self.namepattern = '<TR><TD BGCOLOR="lightblue">%s</TD></TR>\n'
-
+        self.namepattern = '<TR><TD bgcolor="lightblue"><font point-size="16" face="Times-Bold">%s</font></TD></TR>\n'
 
     def addEdge(self, node):
         self.edges.append(node)
@@ -39,27 +38,45 @@ class Node(object):
     def printDotHeader(self):
         print("digraph G {")
         print('  size = "11,17";')
+        print('  ranksep = "1";')
+        print('  rankdir=TB;')
+        self.printTitle()
+        self.staticFormat()
+
+    def staticFormat(self):
+        # this is a result of palying with dot figure and aligning admixes
+        # so that they produce fewer adges and less white space.
+        # create an invisible top-bottom ancoring nodes for fixing
+        # admixes position at specific rank
+        print('{ node [shape=plaintext, style=invis];')
+        print('  11 -> 12 -> 13 -> 14 [style=invis]; };')
+        print('{rank = same; 11; cuda_admix; perl_admix; fileformats_admix; genomics_admix; biotools_admix; chemistry_admix; pytorch_admix};')
+        print('{rank = same; 12; mathlibs_admix; buildlibs_admix; python_admix; foundation_admix; gcc_admix};')
+        print('{rank = same; 13; rust_admix; simulations_admix; R4_admix; imaging_admix; buildtools_admix };')
+        print('{rank = same; 14; tensorflow_admix; systools_admix; yaml2rpm; conda_admix; bioconda_admix; nfsapps_admix; julia_admix};')
 
     def printTitle(self):
-        title = "Software Modules and Dependencies (%s).\n Re-usable packages created with yaml2rpm \n (https://github.com/RCIC-UCI-Public/yaml2rpm)" % self.created
+        title = "Software Modules and Dependencies (%s)\n\nReusable  packages  created  with  yaml2rpm\nhttps://github.com/RCIC-UCI-Public/yaml2rpm" % self.created
         print('  labelloc="t";\n  fontsize="24.0";')
+        print('  fontcolor=dodgerblue4;')
+        print('  fontname="Times-Roman-Bold";')
+        print('  fontsize=24;')
         print('  label="%s";' % title)
 
     def printDotTrailer(self):
-        self.printTitle()
         print("}")
 
     def printDotNotation(self):
-        print("## %s\n" % self.name)
-        reqlist="<TR><TD> requires:\n  <BR/>"
-        provlist="<TR><TD> provides:\n  <BR/>"
+        print("\n## %s" % self.name)
+        reqlist='<TR><TD bgcolor="mintcream"><font color="blue">requires:</font>\n  <BR/>'
+        provlist='<TR><TD bgcolor="ghostwhite"><font color="blue">provides:</font>\n  <BR/>'
         # print ("xxx %s xxx" % self.name)
         if self.requires is not None:
            reqlist="%s%s" % (reqlist, "\n  <BR/>".join(self.requires))
         if self.provides is not None:
            provlist="%s%s" % (provlist, "\n  <BR/>".join(self.provides))
-        reqlist = reqlist + "\n</TD></TR>"       
-        provlist = provlist + "\n</TD></TR>"       
+        reqlist = reqlist + "\n</TD></TR>\n"
+        provlist = provlist + "\n</TD></TR>\n"
         print ('%s [shape=plaintext, %s' % (self.mangleName(),self.labelheader))
         print ('%s%s%s%s]' % (self.namepattern % self.name, reqlist, provlist,self.labeltrailer))
         edgenames = map(lambda x: x.mangleName(), self.edges)
@@ -82,7 +99,7 @@ providers = []
 for am in admixes.keys():
    p = admixes[am]['provides']
    if p is not None:
-      providers.extend([(mod, am) for mod in p])
+       providers.extend([(mod, am) for mod in p])
 
 # Create a dictionary the maps provides to admix names.
 provlist = { x[0]:x[1] for x in providers }
