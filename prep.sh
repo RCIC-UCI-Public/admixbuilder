98005c6d070e6b526b2bc4d17a9dcab2ac570288
Philip M. Papadopoulos <ppapadop@uci.edu>
2024-11-01 11:29:50 -0700

    Make prep.sh executable For Rocky 9, need crb repo for Rocky 8, need powertools repo

--- prep.sh
+++ prep.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# Basic system prep for Rocky 8
+# Basic system prep for Rocky 8/9
+MAJOR=$(cat /etc/redhat-release | sed 's/.*release \([0-9]\+\).*/\1/')
 # Get yumdownloader
 yum -y install yum-utils  
 
 # enable the powertools repo
-yum-config-manager --enable powertools
+
+if [ "$MAJOR" == "8" ]; then 
+    echo "Enabling powertools yum repo"
+    yum-config-manager --enable powertools
+fi
+# enable the crb repo
+if [ "$MAJOR" == "9" ]; then 
+    echo "Enabling powertools crb repo"
+    yum-config-manager --enable crb
+fi
+
 
