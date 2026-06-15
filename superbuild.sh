fd84545fe5a4ce40737ec5c9ff816b6a85f1b37b
Philip M. Papadopoulos <ppapadop@uci.edu>
2026-06-15 09:59:43 -0700

    Small change: clean yum prior to building everything

--- superbuild.sh
+++ superbuild.sh
@@ -2,6 +2,8 @@
 GROUPSDIR=admixgroups
 WORKFILES=$(ls ${GROUPSDIR} | sort)
 echo $WORKFILES
+# clean yum prior to building everything. 
+yum clean all
 for grp in ${WORKFILES}; do
 	PARALLEL=$(wc -l ${GROUPSDIR}/${grp} | awk '{print $1}')
 	echo "=== Start $grp at $(date) ==="
